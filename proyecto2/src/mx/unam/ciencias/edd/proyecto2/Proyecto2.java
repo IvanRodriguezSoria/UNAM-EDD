1868a992fa82303f368c05af422a114d34fa919f
Ivan Rodriguez Soria <vendeta.ivan@hotmail.com>
2016-05-04 10:17:28 -0500

    Mejora Modularizacion

--- proyecto2/src/mx/unam/ciencias/edd/proyecto2/Proyecto2.java
+++ proyecto2/src/mx/unam/ciencias/edd/proyecto2/Proyecto2.java
@@ -1,6 +1,5 @@
 package mx.unam.ciencias.edd.proyecto2;
 
-import mx.unam.ciencias.edd.Lista;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
@@ -10,23 +9,16 @@ public class Proyecto2 {
 
     public static void main(String[] args) throws IOException {
         
-        BufferedReader in;
-        String linea = "";
-        Lista<String> lista = new Lista<String>();
+        LectorEntrada inputReader;
         
         if (args.length > 0)
-            in = new BufferedReader(new FileReader(args[0]));
+            inputReader = new LectorEntrada(new BufferedReader(new FileReader(args[0]) ) );
         else
-            in = new BufferedReader(new InputStreamReader(System.in));
+            inputReader = new LectorEntrada(new BufferedReader(new InputStreamReader(System.in) ) );
             
-        agregaLinea(lista, linea, in);
+        inputReader.agregaLinea();
         
-        for (String s : lista)
+        for (String s : inputReader.lista)
             System.out.println(s);
-    }
-    
-    public static void agregaLinea(Lista<String> lista, String linea, BufferedReader in) throws IOException {
-        while ((linea = in.readLine() ) != null && linea.length() != 0)
-            lista.agrega(linea);
     }
 }
